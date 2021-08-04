f2a6eb131840fa0dedea56b5999e8c67d3c8f92a
uncanny-valley <brandon.fujii.diaz@gmail.com>
2021-08-04 11:52:13 -0500

    Only log virtual memory stats

--- util.py
+++ util.py
@@ -1,18 +1,12 @@
-import os
+import logging
 import psutil
+from psutil._common import bytes2human
 
 
-def log_ram_usage(proc: psutil.Process):
-    logging.info(f'RAM usage: {proc.memory_info()[0]/2. ** 30} GB')
-
-def log_memory_percentage(proc: psutil.Process):
-    logging.info(f'System-wide memory usage percentage: {proc.memory_percent()}')
-
-def log_cpu_percentage(proc: psutil.Process):
-    logging.info(f'System-wide CPU usage percentage: {proc.cpu_percent()}')
-
-def log_memory_usage_summary():
-    p = psutil.Process(os.getpid())
-    log_ram_usage(p)
-    log_memory_percentage(p)
-    log_cpu_percentage(p)
+def log_virtual_memory_stats():
+    mem_usage = psutil.virtual_memory()
+    total = bytes2human(mem_usage.total)
+    available = bytes2human(mem_usage.available)
+    percent_used = mem_usage.percent
+    used = bytes2human(mem_usage.used)
+    logging.info(f'Memory usage: total={total}, available={available}, percent_used={percent_used}%, used={}
