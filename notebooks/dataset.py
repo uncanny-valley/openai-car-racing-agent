bfbe0210016a14af36ba98a78c3cd720d9c46c48
uncanny-valley <brandon.fujii.diaz@gmail.com>
2021-08-13 22:54:17 -0500

    Update dataset script to accept arguments

--- notebooks/dataset.py
+++ notebooks/dataset.py
@@ -1,8 +1,17 @@
+import sys
+sys.path.append("..")
+
+from argparse import ArgumentParser
 import gym
+from gym.envs.box2d import CarRacingV1
 import numpy as np
 import numpy.typing as npt
 import random
+from pyvirtualdisplay import Display
 
+
+display = Display(visible=0, size=(1400, 900))
+display.start()
 
 ACTION_SPACE_ENV_0 = [
     (-1, 1, 0.2), (0, 1, 0.2), (1, 1, 0.2),
@@ -11,14 +20,13 @@ ACTION_SPACE_ENV_0 = [
     (-1, 0,   0), (0, 0,   0), (1, 0,   0)
 ]
 
-def random_action(env_version: int):
+def random_action(env_version: int, env: gym.Env):
     if env_version == 0:
         return env.action_space.sample()
     else:
         action_index = random.randrange(len(ACTION_SPACE_ENV_0))
         return ACTION_SPACE_ENV_0[action_index]
 
-    
 
 def generate_dataset(env_version: int, num_observations: int) -> npt.NDArray[np.float64]:
     if env_version == 0:
@@ -35,21 +43,27 @@ def generate_dataset(env_version: int, num_observations: int) -> npt.NDArray[np.
         )
         dataset = np.empty((num_observations, 96, 96, 4))
 
-    i = 0
-    while i < num_observations:
+    while dataset.shape[0] < num_observations:
         observation = env.reset()
 
         while True:
-            action = random_action(env_version)
+            action = random_action(env_version, env)
             observation, reward, done, _ = env.step(action)
-            print(observation)
-            dataset = dataset.append(observation, axis=0)
+            observation = np.expand_dims(observation, axis=0)
+            dataset = np.append(dataset, observation, axis=0)
 
             if done:
                 break
-    env.close()
 
-    print(dataset)
+    env.close()
+    return dataset
+
 
 if __name__ == '__main__':
-    generate_dataset(env_version=0, num_observations=100)
+    parser = ArgumentParser(description='Generate a dataset of environment state frames')
+    parser.add_argument('-n', '--num-observations', default=100, help='Number of observation frames to generate')
+    parser.add_argument('-e', '--env', default=0, help='The environment version (CarRacing-v0 or CarRacing-v1)')    parser.add_argument('-o', '--output-file', required=True, help='Name of the .npy file to output the dataset')
+    args = parser.parse_args()
+
+    dataset = generate_dataset(env_version=args.env, num_observations=args.num_observations)
+    np.save(args.output_file, dataset)
